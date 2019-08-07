abc7dd3818e6e9427ee8186f53bfc8a103341115
erinla <erin_lawler@yahoo.com>
2019-08-07 14:19:59 -0500

    Updates

---
+++ precache-manifest.c7c38bf4c2e06d6c976bb1cf120a6d57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6b967b2ee3d636ea43a4ba87f8fae1",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "4480a2772402c3a2897d",
+    "url": "/clickygame/static/css/main.9c56ba6a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4480a2772402c3a2897d",
+    "url": "/clickygame/static/js/main.cf91133f.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
